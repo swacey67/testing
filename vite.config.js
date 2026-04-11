db2c0da092001e7173bf26e5aaaf16d23a11c6f1
Fawwaz <fawwazsajid@gmail.com>
2026-04-11 11:41:45 +0700

    Persiapan deploy ke gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Konfigurasi untuk mendapatkan __dirname di environment ESM (type: module)
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // PENTING: Ganti 'testing' dengan nama repository GitHub Anda jika nanti diubah
+  base: '/testing/', 
   plugins: [react()],
   resolve: {
     alias: {
-      // Ini akan membuat simbol '@' langsung mengarah ke folder 'src'
       '@': path.resolve(__dirname, './src'),
     },
   },
